930d3ca3413117d6fb98e24451e23bb914f2669f
Clément LOSCOT <clement.loscot@telecomnancy.eu>
2023-03-15 00:21:23 +0100

    WIP : Vecteur translation triangle rectangle

---
+++ cmake-build-debug/CMakeFiles/sp_engine.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sp_engine.dir/src/main.cpp.obj"
+  "CMakeFiles/sp_engine.dir/src/main.cpp.obj.d"
+  "libsp_engine.dll.a"
+  "sp_engine.exe"
+  "sp_engine.exe.manifest"
+  "sp_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sp_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,23 @@
 #include <GL/glut.h>
 #endif
 
+struct GLPoint {
+    GLfloat posX;
+    GLfloat posY;
+    GLfloat posZ;
+};
+
+struct GLVector {
+    GLPoint a;
+    GLPoint b;
+};
+
+struct GLColor {
+    GLfloat r;
+    GLfloat g;
+    GLfloat b;
+};
+
 void changeSize(int w, int h) {
     if (h == 0)
         h = 1;
@@ -14,19 +31,46 @@ void changeSize(int w, int h) {
     glMatrixMode(GL_MODELVIEW);
 }
 
+void drawTriangleRectangle(GLVector vector, GLColor color, GLboolean sensRect) {
+    glColor3f(color.r,color.g,color.b);
+    glBegin(GL_TRIANGLES);
+    glVertex3f(vector.a.posX, vector.a.posY, vector.a.posZ);
+    glVertex3f(vector.b.posX, vector.b.posY, vector.b.posZ);
+    if (sensRect == 0) {
+        glVertex3f((vector.b.posX - vector.a.posX)*vector.a.posX, (vector.a.posY - vector.b.posY)*vector.b.posY, (vector.b.posZ - vector.a.posZ)*vector.a.posZ);
+    } else {
+        glVertex3f((vector.b.posX - vector.a.posX)*vector.b.posX, (vector.a.posY - vector.b.posY)*vector.a.posY, (vector.b.posZ - vector.a.posZ)*vector.b.posZ);
+    }
+    glEnd();
+}
+
+void drawSquare(GLVector vector, GLColor color1, GLColor color2) {
+    drawTriangleRectangle(vector, color1, 0);
+    drawTriangleRectangle(vector, color2, 1);
+}
+
 float angle = 0.0f;
 
 void render(int) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
 
-    glRotatef(angle, 0.0f, 1.0f, 0.0f);
+    glRotatef(angle, 1.0f, 1.0f, 1.0f);
 
-    glBegin(GL_TRIANGLES);
-    glVertex3f(-1.0f,-1.0f, 0.0f);
-    glVertex3f( 1.0f, 0.0f, 0.0);
-    glVertex3f( 0.0f, 1.0f, 0.0);
-    glEnd();
+    GLPoint a1 = {0.0f,1.0f,0.0f};
+    GLPoint b1 = {1.0f,0.0f,0.0f};
+    GLVector vect1 = {a1,b1};
+    GLColor color11 = {0.0f,1.0f,0.0f};
+    GLColor color12 = {1.0f,0.0f,0.0f};
+
+    GLPoint a2 = {0.0f,1.0f,0.0f};
+    GLPoint b2 = {1.0f,1.0f,1.0f};
+    GLVector vect2 = {a2,b2};
+    GLColor color21 = {0.0f,0.0f,1.0f};
+    GLColor color22 = {1.0f,1.0f,0.0f};
+
+    drawSquare(vect1, color11, color12);
+    drawSquare(vect2, color21, color22);
 
     angle+=1.0f;
     glutSwapBuffers();
