5c09f84e54200f232bd1ed85315655ff7e7969cb
Krabumb <krabumb.gaming@gmail.com>
2023-03-14 17:19:10 +0100

    Ajout tutoriel de démarrage

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,41 @@
-#include <iostream>
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
 
-int main() {
+void changeSize(int w, int h) {
+    if (h == 0)
+        h = 1;
 
-    std::cout << "Hello World !" << std::endl;
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_MODELVIEW);
+}
 
+void render() {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glBegin(GL_TRIANGLES);
+    glVertex3f(-0.5,-0.5,0.0);
+    glVertex3f(0.5,0.0,0.0);
+    glVertex3f(0.0,0.5,0.0);
+    glEnd();
+
+    glutSwapBuffers();
+}
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(300,200);
+    glutInitWindowSize(320,320);
+    glutCreateWindow("SP Engine");
+
+    glutDisplayFunc(render);
+    glutReshapeFunc(changeSize);
+
+    glutMainLoop();
     return 0;
 }
